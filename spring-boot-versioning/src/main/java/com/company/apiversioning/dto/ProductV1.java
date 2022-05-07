036724e7fb2c2e961cb083d337077fd2086c6f35
ParvinValizade <parvin.valizade@mail.ru>
2022-05-07 16:28:05 +0400

    Spring Boot Rest API Versioning

---
+++ spring-boot-versioning/src/main/java/com/company/apiversioning/dto/ProductV1.java
@@ -0,0 +1,12 @@
+package com.company.apiversioning.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductV1 {
+    private String name;
+}
